34dbcaf045971e18a9911d02852e8c37406db79c
XhemoHay <xhemo.haj@gmail.com>
2023-10-26 07:37:34 +0200

    vdv

--- index.js
+++ index.js
@@ -42,18 +42,20 @@ async function main() {
  
 }
 
-app.use(express.static(path.join(__dirname, "public")));
 
-// Define the catch-all route to serve the index.html
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 app.use("/api/auth", authRoter);
 // app.use("/api/users", userRoter);
 app.use("/api/students", studentRoter);
 app.use("/api/salah", salahRoter);
 app.use("/api/klasat", classRoter);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define the catch-all route to serve the index.html
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({ message: "Route not found" });
